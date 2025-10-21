fe4879cce41ae7bd9c4bc426eddf9777bd727d4c
BuildTools <unconfigured@null.spigotmc.org>
2025-10-21 10:54:08 -0500

    Initialization, Basic View Configuration

---
+++ fsied/src/extension.ts
@@ -0,0 +1,15 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { EdMainMenu } from './edmenu';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	const rootPath = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0)) ? vscode.workspace.workspaceFolders[0].uri.fsPath : "";
+	const mainMenuProvider = new EdMainMenu(rootPath);
+	vscode.window.registerTreeDataProvider('mainmenu', mainMenuProvider);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
